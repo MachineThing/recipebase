840b1c2191f136d5b47f63739cbaf82c45c88a46
Mason Fisher <sackzombie10@gmail.com>
2023-11-10 16:43:39 -0500

    Made animated button

--- recipe-fe/src/app/components/navbar.tsx
+++ recipe-fe/src/app/components/navbar.tsx
@@ -5,20 +5,35 @@ import Link from 'next/link';
 
 export const Navbar = () => {
     const [isClicked, setClicked] = useState(false);
-
+    const prefixClasses = "transform duration-100 ease-linear"
+    
     const handleClick = () => {
         setClicked(!isClicked)
-        console.log(isClicked)
+        console.log(isClicked ? "closed menu" : "opened menu")
+    }
+    
+    const genPath = (y: number, reversed?: boolean) => {
+        return isClicked ? `M2 ${y} L11.5 11.5 L22 ${y}` : `M2 ${y} L11.5 ${y} L22 ${y}`
     }
 
+    let isClickedNum: number = (isClicked ? 1 : 0)
+
     return (
-        <nav className="bg-white p-4 sticky top-0 shadow-lg">
+        <nav className="bg-white p-1 sticky top-0 shadow-lg">
             <div className="text-xl">
-                <button className="mr-4 inline-block align-middle" onClick={handleClick}>
+                <button className="mr-2 p-3 inline-block align-middle" onClick={handleClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="23" height="23">
-                        <rect width="24" height="4" rx="2" ry="2"/>
-                        <rect width="24" height="4" y="10" rx="2" ry="2"/>
-                        <rect width="24" height="4" y="20" rx="2" ry="2"/>
+                        {/* top line    */}
+                        <path className={prefixClasses}
+                            stroke-linecap="round" stroke-width="4" stroke="black" fill="none" d={genPath(3)}/>
+                        
+                        {/* middle line */}
+                        <rect className={prefixClasses}
+                            x="0" y={10+1.5*isClickedNum} width="24" height={4-4*isClickedNum} rx="2" />
+                        
+                        {/* bottom line */}
+                        <path className={prefixClasses}
+                            stroke-linecap="round" stroke-width="4" stroke="black" fill="none" d={genPath(21)}/>
                     </svg>
                 </button>
                 <Link href="/" className="font-semibold select-none align-middle">Recipebase</Link>
