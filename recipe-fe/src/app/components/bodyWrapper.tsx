3a058982ad6b8e7f99b49a60d3501158ad76af96
Mason Fisher <sackzombie10@gmail.com>
2023-11-14 07:35:32 -0500

    Clicking on overlay closes navbar menu

--- recipe-fe/src/app/components/bodyWrapper.tsx
+++ recipe-fe/src/app/components/bodyWrapper.tsx
@@ -3,9 +3,12 @@ import { ReactNode, useState } from 'react';
 
 import { Navbar } from './navbar';
 
-const Overlay = (props: {enabled: Boolean}) => {
+const Overlay = (props: {enabled: Boolean, setOpen: Function}) => {
+    const onClick = () => {
+        props.setOpen(false)
+    }
     return (
-        <div className={"z-10 h-screen w-screen fixed bg-black transform duration-100 ease-linear " + (props.enabled ? "opacity-65 cursor-pointer" : "pointer-events-none opacity-0")}></div>
+        <div className={"z-10 h-screen w-screen fixed bg-black transform duration-100 ease-linear " + (props.enabled ? "opacity-65 cursor-pointer" : "pointer-events-none opacity-0")} onClick={onClick}></div>
     )
 }
 
@@ -13,7 +16,7 @@ export const BodyWrapper = (props: {children: ReactNode}) => {
     const [navOpen, setOpen] = useState(false);
     return (
         <>
-        <Overlay enabled={navOpen}/>
+        <Overlay enabled={navOpen} setOpen={setOpen}/>
         <Navbar navOpen={navOpen} setOpen={setOpen}></Navbar>
         <div className="p-2 z-0">{props.children}</div>
         </>
